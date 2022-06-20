b9ddf968b092569f90bd120bad5ecada40f9bf2f
Tom Cousin <tom.cousin@cdbdx.biz>
2022-06-20 16:34:41 +0200

    Add DiceRoll Tests

--- Yahtzee/Tests/DiceRollTests.cs
+++ Yahtzee/Tests/DiceRollTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System.Linq;
+using System.Collections.Generic;
+
 using Xunit;
 
 namespace Tests
@@ -12,13 +13,37 @@ namespace Tests
         {
             Assert.Throws<ArgumentException>(() => new DiceRoll(1, 2, 3, 4, 5, 6));
         }
+
+        [Fact]
+        public void Should()
+        {
+            // Arrange
+            var expected = new[] { 1, 2, 3, 4, 5 };
+            var diceRoll = new DiceRoll(1, 2, 3, 4, 5);
+
+            // Act
+            var roll = diceRoll.GetRoll();
+
+            // Assert
+            Assert.True(expected.Intersect(roll).Count() == 5);
+        }
     }
 
     public class DiceRoll
     {
+        private readonly int[] _roll;
+
         public DiceRoll(params int[] rolls)
         {
-            throw new ArgumentException();
+            if (rolls.Length != 5)
+                throw new ArgumentException();
+
+            _roll = rolls;
+        }
+
+        public IEnumerable<int> GetRoll()
+        {
+            return _roll.ToList();
         }
     }
 }
