d3f4d96fdd2da44fc95b319508009c1747a100c4
yasmine.amara <yasmine.amara@cdiscount.com>
2022-06-20 16:20:08 +0200

    create DiceRoll class

---
+++ Yahtzee/Tests/DiceRollTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Xunit;
+
+namespace Tests
+{
+    public class DiceRollTests
+    {
+        [Fact]
+        public void Should_return_sum_of_dice_when_chance_combination_invoked()
+        {
+            Assert.Throws<ArgumentException>(() => new DiceRoll(1, 2, 3, 4, 5, 6));
+        }
+    }
+
+    public class DiceRoll
+    {
+        public DiceRoll(params int[] rolls)
+        {
+            throw new ArgumentException();
+        }
+    }
+}
