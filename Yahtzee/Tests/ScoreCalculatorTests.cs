999025d9eec4c73aafbc2682126292cff79b9fe9
yasmine.amara <yasmine.amara@cdiscount.com>
2022-06-20 12:43:29 +0200

    three of a kind test

--- Yahtzee/Tests/ScoreCalculatorTests.cs
+++ Yahtzee/Tests/ScoreCalculatorTests.cs
@@ -27,7 +27,12 @@ namespace Tests
 
         [Theory]
         [InlineData(1,new [] { 1, 3, 4, 6, 6 },1)]
-        public void Should_return_the_sum_of_dice_of_the_combination_value_for_simple_combinations(int combination, IEnumerable<int> rolls, int expectedScore)
+        [InlineData(2,new [] { 1, 3, 4, 2, 2 },4)]
+        [InlineData(3,new [] { 1, 3, 4, 2, 2 },3)]
+        [InlineData(4,new [] { 1, 3, 4, 2, 2 },4)]
+        [InlineData(5,new [] { 1, 3, 5, 5, 5 },15)]
+        [InlineData(6,new [] { 1, 3, 6, 2, 6 },12)]
+        public void Should_return_the_sum_of_dice_of_the_combination_value_for_upper_section_combinations(int combination, IEnumerable<int> rolls, int expectedScore)
         {
             // Arrange;
 
@@ -38,17 +43,17 @@ namespace Tests
             Assert.Equal(expectedScore, score);
         }
 
-        [Fact]
-        public void Should_return_sum_of_two_when_containing_only_one_two()
+        [Theory]
+        [InlineData(7,new [] { 1, 1, 1, 4, 5 },12)]
+        public void Should(int combination, IEnumerable<int> rolls, int expectedScore)
         {
-            // Arrange
-            var dice = new List<int> { 1, 2, 4, 6, 6 };
+            // Arrange;
 
             // Act
-            var score = _calculator.GetScore(dice, 2);
+            var score = _calculator.GetScore(rolls.ToList(), combination);
 
             // Assert
-            Assert.Equal(2, score);
+            Assert.Equal(expectedScore, score);
         }
     }
 }
