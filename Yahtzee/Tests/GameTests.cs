e370eb3ccda6d029041cd3ede434d7c1f69e5573
yasmine.amara <yasmine.amara@cdiscount.com>
2022-06-21 11:11:14 +0200

    combination aces score

--- Yahtzee/Tests/GameTests.cs
+++ Yahtzee/Tests/GameTests.cs
@@ -22,38 +22,21 @@ namespace Tests
             score.Should().Be(expected);
         }
 
-        [Fact]
-        public void Should_return_total_1_when_aces_scores_1_point()
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5},1)]
+        [InlineData(new int[] { 1, 1, 1, 1, 5},4)]
+        [InlineData(new int[] { 2, 3, 3, 4, 5},0)]
+        public void Should_return_expected_total_when_only_scores_for_combination_aces(int[] roll, int expected)
         {
             // Arrange
             var game = new Game();
-            var expected = 1;
 
             // Act
-            game.Score(Combination.Aces, new DiceRoll(1, 2, 3, 4, 5));
+            game.Score(Combination.Aces, new DiceRoll(roll));
             var score = game.GetUpperSectionTotal();
 
             // Assert
             score.Should().Be(expected);
         }
-    }
-
-    internal class Game
-    {
-        public Game()
-        {
-        }
-
-        public int GetUpperSectionTotal()
-        {
-            return score;
-        }
-
-        public void Score(Combination aces, DiceRoll diceRoll)
-        {
-            score += 1;
-        }
-
-        private int score = 0;
     }
 }
---
+++ Yahtzee/Yahtzee/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace Yahtzee
+{
+    public class Game
+    {
+        private int score = 0;
+
+        public int GetUpperSectionTotal()
+        {
+            return score;
+        }
+
+        public void Score(Combination aces, DiceRoll diceRoll)
+        {
+            ScoreCalculator scoreCalculator = new ScoreCalculator();
+            score += scoreCalculator.GetScore(diceRoll, aces);
+        }
+    }
+}
