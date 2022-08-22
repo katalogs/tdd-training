593d91e37b419d4805e9bb50b7100490f4af0b4d
Kinda Al Chahid <kinda.alchahid@cdiscount.com>
2022-08-22 15:40:41 +0200

    wip

--- Yahtzee/Tests/CalculatorTest.cs
+++ Yahtzee/Tests/CalculatorTest.cs
@@ -8,18 +8,27 @@ namespace Tests
         [Fact]
         public void Aces_with_five_1_should_return_5()
         {
-            // But test : Renvoie de la bonne valeur du cas 5 dés de valeur 1
-
             // Arrange
             var calculator = new Calculator();
 
             // Act
-            int combination = 1;
-
-            int actual = calculator.GetTotal(combination, new int[] { 1, 1, 1, 1, 1 });
+            var actual = calculator.GetTotal(1, new[] { 1, 1, 1, 1, 1 });
 
             // Assert
             Assert.Equal(5, actual);
+        }
+        
+        [Fact]
+        public void Aces_with_three_1_should_return_3()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actual = calculator.GetTotal(1, new[] { 1, 1, 1, 2, 5 });
+
+            // Assert
+            Assert.Equal(3, actual);
         }
     }
 }
--- Yahtzee/Yahtzee/Calculator.cs
+++ Yahtzee/Yahtzee/Calculator.cs
@@ -1,10 +1,13 @@
-﻿namespace Tests
+﻿using System.Linq;
+
+namespace Tests
 {
     public class Calculator
     {
         public int GetTotal(int combination, int[] dices)
         {
-            return 5;
+            var list = dices.ToList();
+            return list.Count(x => x == 1);
         }
     }
 }
