7c023962f77228da387c77d95f981e1c8e043ade
Mathieu RICHARD <mathieu.richard@cdbdx.biz>
2022-08-23 12:22:59 +0200

    Test ScoreBoard score 0

--- Yahtzee/Tests/ScoreBoardTests.cs
+++ Yahtzee/Tests/ScoreBoardTests.cs
@@ -5,11 +5,26 @@ namespace Tests
 {
     public class ScoreBoardTests
     {
-        /*
+
         [Fact]
-        public void Add_10_points_to_global_score(List<int> dices, int expected, int combination)
+        public void On_game_start_should_return_score_0()
         {
+            //1 - Score 0
+            //2 - Test de l'ensemble des combinaisons (vérifier feuille blanche)
+
+            // Arrange
+            var scoreBoard = new ScoreBoard();
+
+            var combination = 1;
+            var dices = new List<int>();
+
+            // Act
+            var actual = scoreBoard.CalculateTotalBeforeBonus(combination, dices);
+
+            // Assert
+            Assert.Equal(0, actual);
+
         }
-        */
+
     }
 }
