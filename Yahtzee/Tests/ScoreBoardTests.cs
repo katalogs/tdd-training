24d9c8d027b018ee02e328f72089b64db5a21cd8
Mathieu RICHARD <mathieu.richard@cdbdx.biz>
2022-08-23 15:20:08 +0200

    Test combinaison 41

--- Yahtzee/Tests/ScoreBoardTests.cs
+++ Yahtzee/Tests/ScoreBoardTests.cs
@@ -42,6 +42,10 @@ namespace Tests
         [Theory]
         [InlineData(1)]
         [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
         public void Add_same_combination_twice_should_throw_exception(int combination)
         {
             // Arrange
@@ -51,5 +55,28 @@ namespace Tests
             // Act
             Assert.Throws<SameCombinationTwiceException>(() => scoreBoard.AddCombination(combination, new List<int> { 4, 2, 3, 1, 3 }));
         }
+
+        //Aces 3
+        //Twos 8
+        //Fours 12
+        //Sixes 18
+        //Total 41  
+        [Fact]
+        public void Combination_not_fully_set_should_return_score_41()
+        {
+            // Arrange
+            var scoreBoard = new ScoreBoard();
+            scoreBoard.AddCombination(1, new List<int> { 1, 1, 1, 2, 5 });
+            scoreBoard.AddCombination(2, new List<int> { 2, 2, 2, 2, 5 });
+            scoreBoard.AddCombination(4, new List<int> { 4, 4, 4, 1, 2 });
+            scoreBoard.AddCombination(6, new List<int> { 6, 6, 6, 1, 2 });
+
+            // Act
+            var actual = scoreBoard.CalculateTotalBeforeBonus();
+
+            // Assert
+            Assert.Equal(41, actual);
+        }
+
     }
 }
